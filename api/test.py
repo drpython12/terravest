4db08492d5a991070ef3c5baa62dd4f0f62566d2
Palash Gandhi <palashsamirgandhi@gmail.com>
2025-04-06 21:00:48 +0100

    Admin changes.

--- api/test.py
+++ api/test.py
@@ -1,13 +1,2 @@
-import http.client
-
-conn = http.client.HTTPSConnection("yahoo-finance-real-time1.p.rapidapi.com")
-
-headers = {
-    'x-rapidapi-key': "c4cf9f510cmsh80ee50ea6a7d2b3p171c27jsnab5350889c90",
-    'x-rapidapi-host': "yahoo-finance-real-time1.p.rapidapi.com"
-}
-
-conn.request("GET", "/stock/get-quote-summary?symbol=WOOF&lang=en-US&region=US", headers=headers)
-
-res = conn.getresponse()
-print(res.read().decode("utf-8"))
+import googleNewsScraper from 'google-news-scraper';
+const articles = await googleNewsScraper({ searchTerm: "Apple Inc ESG OR Environmental OR Social OR Governance OR Sustainability" });
